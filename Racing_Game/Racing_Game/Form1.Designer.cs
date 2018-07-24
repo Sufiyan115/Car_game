134eb47e81d2cde48a9174baa28655bc3a774e30
Rajeel Zahid <41546623+RajeelZahid@users.noreply.github.com>
2018-07-24 08:49:42 -0700

    cargame_sufiyan

---
+++ Racing_Game/Racing_Game/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Racing_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.car2_mover = new System.Windows.Forms.Timer(this.components);
+            this.car3_mover = new System.Windows.Forms.Timer(this.components);
+            this.car4_mover = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(45, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(12, 76);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(164, -3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(12, 76);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(45, 94);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(12, 76);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(164, 94);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(12, 76);
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(45, 191);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(12, 76);
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(164, 191);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(12, 76);
+            this.pictureBox6.TabIndex = 1;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(164, 288);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(12, 76);
+            this.pictureBox7.TabIndex = 3;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(45, 288);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(12, 76);
+            this.pictureBox8.TabIndex = 2;
+            this.pictureBox8.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
+            this.pictureBox9.Location = new System.Drawing.Point(90, 261);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(30, 60);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 4;
+            this.pictureBox9.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox10.Image")));
+            this.pictureBox10.Location = new System.Drawing.Point(0, 41);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(30, 60);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 5;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox11.Image")));
+            this.pictureBox11.Location = new System.Drawing.Point(178, 224);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(30, 60);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 6;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox12.Image")));
+            this.pictureBox12.Location = new System.Drawing.Point(90, -31);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(30, 60);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 7;
+            this.pictureBox12.TabStop = false;
+            // 
+            // car2_mover
+            // 
+            this.car2_mover.Tick += new System.EventHandler(this.car2_mover_Tick);
+            // 
+            // car3_mover
+            // 
+            this.car3_mover.Tick += new System.EventHandler(this.car3_mover_Tick);
+            // 
+            // car4_mover
+            // 
+            this.car4_mover.Tick += new System.EventHandler(this.car4_mover_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(60, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 33);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 361);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Timer car2_mover;
+        private System.Windows.Forms.Timer car3_mover;
+        private System.Windows.Forms.Timer car4_mover;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Racing_Game/Racing_Game/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Racing_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int score, speed;
+       
+
+        //declaring array to store objects
+        PictureBox[] road = new PictureBox[8];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            speed = 2;
+            score = 0;
+            //form size
+            this.Width = 240;
+            this.Height = 400;
+
+            this.Text = "Car Race";
+
+            //disabling maximize / minimize box
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            //MinimumSize.Width = 240;
+            //MinimumSize.Height = 240;
+
+            //gameover label
+            this.label1.BackColor = Color.White;
+            this.label1.ForeColor = Color.Red;
+            this.label1.Text = "Game Over";
+            this.label1.Visible = false;
+            this.label1.Font = new Font(label1.Font.FontFamily, 16,FontStyle.Bold);
+
+            //score label1
+            this.label2.Text = "Score: 0";
+            this.label2.ForeColor = Color.White;
+            this.label2.Font = new Font(label2.Font.FontFamily, 10, FontStyle.Bold);
+
+            //speed label
+            this.label4.Text = "Speed: " + speed;
+            this.label4.ForeColor = Color.White;
+            this.label4.Font = new Font(label4.Font.FontFamily, 10, FontStyle.Bold);
+
+
+            //about label
+            this.label3.Text = "Developed By: M.Sufiyan";
+            
+            this.label3.TextAlign = ContentAlignment.MiddleCenter;
+            this.label3.Font = new Font("Times", 10, FontStyle.Bold);
+            this.label3.ForeColor = Color.Black;
+            this.label3.Visible = false;
+
+            //replay button
+            this.button1.Text = "Replay";
+            this.button1.ForeColor = Color.White;
+            this.button1.BackColor = Color.DarkBlue;
+            this.button1.Visible = false;
+            this.button1.Font = new Font(button1.Font.FontFamily, 10);
+
+            //Quit game button
+            this.button2.Text = "Quit Game";
+            this.button2.ForeColor = Color.White;
+            this.button2.BackColor = Color.Red;
+            this.button2.Visible = false;
+            this.button2.Font = new Font(button2.Font.FontFamily, 10);
+
+            //color
+            this.BackColor = Color.DarkGray;
+            this.pictureBox1.BackColor = Color.White;
+            this.pictureBox2.BackColor = Color.White;
+            this.pictureBox3.BackColor = Color.White;
+            this.pictureBox4.BackColor = Color.White;
+            this.pictureBox5.BackColor = Color.White;
+            this.pictureBox6.BackColor = Color.White;
+            this.pictureBox7.BackColor = Color.White;
+            this.pictureBox8.BackColor = Color.White;
+
+            //road timer
+            this.timer1.Interval=10;
+            this.timer1.Enabled = true;
+
+            //rightkey press timer
+            this.timer2.Interval = 10;
+            this.timer2.Enabled = false;
+
+            //left key press timer
+            this.timer3.Interval = 10;
+            this.timer3.Enabled = false;
+
+            //enemy car 1 timer
+            this.car2_mover.Interval = 10;
+            this.car2_mover.Enabled = true;
+
+            //enemy car 2 timer
+            this.car3_mover.Interval = 10;
+            this.car3_mover.Enabled = true;
+
+            //enemy car 3 timer
+            this.car4_mover.Interval = 10;
+            this.car4_mover.Enabled = true;
+
+            this.AcceptButton = button1;
+            this.CancelButton = button2;
+            //assigning roadstraps into array
+            road[0] = pictureBox1;
+            road[1] = pictureBox2;
+            road[2] = pictureBox3;
+            road[3] = pictureBox4;
+            road[4] = pictureBox5;
+            road[5] = pictureBox6;
+            road[6] = pictureBox7;
+            road[7] = pictureBox8;
+
+         }
+
+        //moving road
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            for (int x = 0; x <= 7; x++)
+            {
+                road[x].Top += speed;
+
+                //for repating road movement
+                if (road[x].Top>=this.Height)
+                {
+                    road[x].Top = -road[x].Height;
+                }
+              
+            }
+            if (pictureBox9.Bounds.IntersectsWith(pictureBox10.Bounds))
+            {
+                gameover();
+            }
+            if (pictureBox9.Bounds.IntersectsWith(pictureBox11.Bounds))
+            {
+                gameover();
+            }
+            if (pictureBox9.Bounds.IntersectsWith(pictureBox12.Bounds))
+            {
+                gameover();
+            }
+            //increasing speed
+            if(score>10 && score<20)
+            {
+                speed = 3;
+                this.label4.Text = "Speed: " + speed;
+            }
+            if (score > 20 && score < 30)
+            {
+                speed = 4;
+                this.label4.Text = "Speed: " + speed;
+            }
+            if (score > 30)
+            {
+                speed = 5;
+                this.label4.Text = "Speed: " + speed;
+            }
+        }
+        private void gameover()
+        {
+            this.label1.Visible = true;
+            this.button1.Visible = true;
+            this.button2.Visible = true;
+            this.timer1.Stop();
+            this.label3.Visible = true;
+
+            this.label2.ForeColor = Color.Yellow;
+            
+            this.car2_mover.Stop();
+            this.car3_mover.Stop();
+            this.car4_mover.Stop();
+        }
+        private void increseSpeed()
+        {
+            score += 1;
+            label2.Text = "Score: " + score;
+        }
+ 
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //starting timer when user press right key
+            if (e.KeyCode == Keys.Right)
+            {
+                timer2.Start();
+            }
+
+            //starting timer when user press left key
+            if (e.KeyCode == Keys.Left)
+
+            {
+                timer3.Start();
+            }
+        }
+
+        //moving player car to right on timer start
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(pictureBox9.Location.X<182)
+            pictureBox9.Left += 5;
+        }
+
+        //moving player car to left on timer start
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox9.Location.X >5)
+                pictureBox9.Left -= 5;
+
+        }
+
+        //stoping timer when user release right/left key
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            timer2.Stop();
+            timer3.Stop();
+        }
+
+
+        public  Random r = new Random();
+        //moving enemy car 1
+        private void car2_mover_Tick(object sender, EventArgs e)
+        {
+            pictureBox10.Top += speed * 3 / 2;
+
+            //repeting when car cross the form (YELLOW CAR)
+            if(pictureBox10.Top>=this.Height)
+            {
+                //    int x = r.Next(0, 240);
+                //  int y = r.Next(0, 400);
+                
+
+                //increasing score when enemy car crosses the form every time , with the help on funtion
+                increseSpeed();
+                //moving car on random position 
+                pictureBox10.Location = new Point((int)(new Random().Next(3,60)), -100);
+
+
+                //pictureBox10.Top = -(y + pictureBox10.Height) * -1;
+                //pictureBox10.Left = -(x * 240) + pictureBox10.Height;
+               // pictureBox10.Top = -(new Random().Next(0,150)+pictureBox10.Height);
+                //pictureBox10.Left = -(new Random().Next(0, 240));
+                //pictureBox10.Top = ((int.Parse(Math.Ceiling(Random(1,240)) + pictureBox10.Height)* -1);
+            }
+        }
+
+        //moving enemy car 2 (RED CAR)
+        private void car3_mover_Tick(object sender, EventArgs e)
+        {
+            pictureBox11.Top += speed * 5 / 3;
+
+
+            //repeting when car cross the form
+            if (pictureBox11.Top >= this.Height)
+            {
+                increseSpeed();
+                pictureBox11.Location = new Point((int)(new Random().Next(63, 127)), -100);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            InitializeComponent();
+            Form1_Load(e, e);
+        }
+
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //moving enemy car 3 (green car)
+        private void car4_mover_Tick(object sender, EventArgs e)
+        {
+            pictureBox12.Top += speed * 3 / 2;
+
+            //repeting when car cross the form
+            if (pictureBox12.Top >= this.Height)
+            {
+                //increseSpeed();
+                pictureBox12.Location = new Point((int)(new Random().Next(130, 190)), -100);
+            }
+        }
+    }
+}
